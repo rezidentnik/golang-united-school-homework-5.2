8440f673c6ea1fd4d1f1406332ee61c4f20a19d6
admin <admin@admin.net>
2022-05-19 22:37:40 +0300

    add solution

--- cache.go
+++ cache.go
@@ -1,23 +1,60 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type cacheDatum struct {
+	value          string
+	expirationTime time.Time
+}
 
 type Cache struct {
+	data map[string]cacheDatum
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache *Cache) Get(key string) (string, bool) {
+	cachedValue, isValueExist := cache.data[key]
 
+	if !isValueExist {
+		return "", false
+	}
+
+	if !cachedValue.expirationTime.After(time.Now()) {
+		delete(cache.data, key)
+		return "", false
+	}
+
+	return cachedValue.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.data[key] = cacheDatum{value: value}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Keys() []string {
+	cacheSize := len(cache.data)
+	keys := make([]string, cacheSize)
+	i := 0
+	for key, datum := range cache.data {
+		if datum.expirationTime.After(time.Now()) {
+			keys[i] = key
+			i++
+		} else {
+			delete(cache.data, key)
+		}
+	}
+	numberOfExpiredValues := cacheSize - i
+
+	return keys[0 : cacheSize-numberOfExpiredValues]
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	if deadline.After(time.Now()) {
+		cache.data[key] = cacheDatum{value: value, expirationTime: deadline}
+	}
 }
